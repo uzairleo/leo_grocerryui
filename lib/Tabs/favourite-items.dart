6584b7e5e7fed7a707fcb1f4a6ec4508cdb23bd2
Uzair Leo <uzair.jan336@gmail.com>
2020-06-11 18:13:25 +0500

    created all Tabs demo

---
+++ lib/Tabs/favourite-items.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FavouriteItems extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("favourite items"),
+    );
+  }
+}
---
+++ lib/Tabs/user-account.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class UserAccount extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("User Accout"),
+    );
+  }
+}
