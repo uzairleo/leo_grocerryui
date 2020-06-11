6b5f0fdfea9380107f6472cd252be9275564f263
Uzair Leo <uzair.jan336@gmail.com>
2020-06-12 01:12:32 +0500

    done store.dart

--- lib/Tabs/store.dart
+++ lib/Tabs/store.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
-
+import '../constants/constants.dart';
+import '../widgets/widgets.dart';
 
 class Store extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Store"),
-    );
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return Container(
+        width: width,
+        height: height,
+        child: Stack(children: <Widget>[
+          StoreWidget.center(),
+          StoreWidget.header(),
+        ]));
   }
 }
